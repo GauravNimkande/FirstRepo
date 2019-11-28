35534ec33f5f933ffc606c4c8cf933205f94055c
Admin <Admin@Admin-PC>
2019-11-28 05:54:52 +0530

    fdf

--- Sample/src/Interviewprograms/Countrepitint.java
+++ Sample/src/Interviewprograms/Countrepitint.java
@@ -6,14 +6,16 @@ public class Countrepitint {
 		// TODO Auto-generated method stub
 		
 		
-		int A[]={1,3,4,5,1,3,4};
-		
+		int A[]={1,3,4,5,1,3,4,5};
+		int counter=0;
 		for(int i=0;i<A.length;i++){
 			for(int j=i+1;j<A.length;j++){
 		
 				if(A[i]==A[j]){
-					System.out.println("\n"+ A[j]);
+					counter++;
+					System.out.println("\n"+ A[j]+"Repets : "+counter+" Times");
 				
+					
 				}
 				
 			}
